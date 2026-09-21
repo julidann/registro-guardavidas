fc1b5c6a5e6809b6c8959178efa1331fe4b84b96
Julieta D'Annunzio <169107108+julidann@users.noreply.github.com>
2026-09-21 22:13:46 +0000

    Agrega cache basica para uso como PWA

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "registro-guardavidas-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
